e2dc82bf9fced5f849295d59e5154fb5a427df8a
kasemsun <kasemsun4869@gmail.com>
2023-09-09 12:05:46 +0700

    Fix responsive btn location

--- src/component/Search.tsx
+++ src/component/Search.tsx
@@ -21,23 +21,24 @@ const Search = ({
         </p>
       
       <div className="relative mt-10">
-        <input
-          type="text"
-          className="px-2 rounded-l-md border-2 border-white py-1 
-            md:w-[350px] lg:w-[500px] xl:w-[700px]"
-          onChange={onInputChange}
-          value={term}
-        />
-
-        <button
-          className="rounded-r-md border-2 border-white hover:border-zinc-500s hover:text-zinc-500 text-zinc-1 px-2 py-1 cursor-pointer text-white"
-          onClick={onSearch}
-        >
-          search
-        </button>
-        <button className="ml-1" onClick={onCurrentLocation}>
-          <FontAwesomeIcon size="xl" color="white" icon={faLocation} />
-        </button>
+      <div className="flex items-center">
+  <input
+    type="text"
+    className="px-2 rounded-l-md border-2 border-white py-1 
+    md:w-[350px] lg:w-[500px] xl:w-[700px] flex-grow-1" // Added flex-grow-1
+    onChange={onInputChange}
+    value={term}
+  />
+  <button
+    className="rounded-r-md border-2 border-white hover:border-zinc-500s hover:text-zinc-500 text-zinc-1 px-2 py-1 cursor-pointer text-white"
+    onClick={onSearch}
+  >
+    Search
+  </button>
+  <button className="ml-1" onClick={onCurrentLocation}>
+    <FontAwesomeIcon size="xl" color="white" icon={faLocation} />
+  </button>
+</div>
         <ul className="absolute top-9 bg-white ml-l rounded-b-md">
           {searchOption.map((option: optionType, index: number) => (
             <li key={index} >
